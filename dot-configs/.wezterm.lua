5d7d17a44b9763d0aabb3d97309279503030087f
Sagar Sabu <chelekadusagar@gmail.com>
2025-03-26 16:06:35 +1300

    add some dot configs

---
+++ dot-configs/.wezterm.lua
@@ -0,0 +1,10 @@
+-- Pull in the wezterm API
+local wezterm = require 'wezterm'
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+config.font = wezterm.font 'JetBrains Mono'
+config.enable_scroll_bar = true
+
+return config
