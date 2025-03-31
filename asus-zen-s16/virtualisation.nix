fe867e8cc2d40b1f5b74fed31fea415102ce278b
Sagar Sabu <chelekadusagar@gmail.com>
2025-03-31 21:23:41 +1300

    don't use rootless docker

--- asus-zen-s16/virtualisation.nix
+++ asus-zen-s16/virtualisation.nix
@@ -3,7 +3,7 @@
   # docker
   virtualisation.docker = {
     enable = true;
-    rootless.enable = true;
-    rootless.setSocketVariable = true;
+    rootless.enable = false;
   };
+  virtualisation.containerd.enable = true;
 }
