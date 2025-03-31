d6491928e71dd9ba0ce5e5c077a70101c1e7bdc9
Sagar Sabu <chelekadusagar@gmail.com>
2025-03-31 21:51:07 +1300

    fix fish aliases for build container

--- dotfiles/.work_bash_aliases.fish
+++ dotfiles/.work_bash_aliases.fish
@@ -4,18 +4,29 @@
 
 set -gx _WORKSPACE "$HOME/tworkspace"
 
+function ensure-dir-exists -a _dir
+    if test ! -d "$_dir"
+        mkdir -p "$_dir"
+    end
+end
+
 function build-p25-docker-u22
     set -l _uid (uuidgen -t)
     set -l _container_name "p25-builder-u22-"(string sub -l 8 $_uid)
     set -l _docker_image gitlab.taitradio.net:4567/core-network/tn9400/p25core/p25-builder-ubuntu22-04:03-34
+    set -l _docker_socket_vol /var/run/docker.sock:/var/run/docker.sock
     set -l _grp_vol /etc/group:/etc/group:ro
     set -l _pwd_vol /etc/passwd:/etc/passwd:ro
     set -l _shadow_vol /etc/shadow:/etc/shadow:ro
-    set -l _docker_socket_vol /var/run/docker.sock:/var/run/docker.sock
+    set -l _npm_vol "$HOME/.npm/:$HOME/.npm/"
+    set -l _docker_vol "$HOME/.docker/:$HOME/.docker/"
     set -l _workspace_vol "$_WORKSPACE:$_WORKSPACE"
-    set -l _work_dir "$_WORKSPACE"
     set -l _container_hostname u22-builder
+    set -l _work_dir "$_WORKSPACE"
     set -l _docker_grp_id (getent group docker | awk -F: '{print $3}')
+
+    ensure-dir-exists "$HOME/.npm"
+    ensure-dir-exists "$HOME/.docker"
 
     # docker pull $_docker_image
 
@@ -32,6 +43,8 @@ function build-p25-docker-u22
         -v $_grp_vol \
         -v $_pwd_vol \
         -v $_shadow_vol \
+        -v $_npm_vol \
+        -v $_docker_vol \
         -v $_docker_socket_vol \
         -v $_workspace_vol \
         $_docker_image \
