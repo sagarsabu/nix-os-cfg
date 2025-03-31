c868b2051d5782a5527e7facb887ea88b82c1d45
Sagar Sabu <chelekadusagar@gmail.com>
2025-03-31 22:21:57 +1300

    fix wezterm missing title bar icons

--- dotfiles/.wezterm.lua
+++ dotfiles/.wezterm.lua
@@ -7,5 +7,9 @@ local config = wezterm.config_builder()
 config.font = wezterm.font 'JetBrains Mono'
 config.font_size = 12.0
 config.enable_scroll_bar = true
+config.audible_bell = "Disabled"
+
+-- Fix missing title bar icons and inability to move the terminal
+config.enable_wayland = false
 
 return config
