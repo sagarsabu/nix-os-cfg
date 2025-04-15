d3c597bf8f8758c155ef0c0c44846f8505f0a281
Sagar Sabu <chelekadusagar@gmail.com>
2025-04-16 00:04:01 +1200

    update latest nix packages to truly fix audio

--- asus-zen-s16/audio.nix
+++ asus-zen-s16/audio.nix
@@ -10,43 +10,42 @@
     alsa.support32Bit = true;
     pulse.enable = true;
 
-    # try fix sub-wuffer
     # from "https://github.com/BNieuwenhuizen/zenbook-s16"
-
-    extraConfig.pipewire-pulse = {
-      "99-speaker-routing" = {
-        "context.modules" = [
-          {
-            "name" = "libpipewire-module-loopback";
-            "args" = {
-              "node.description" = "Stereo to 4.0 upmix";
-              "audio.position" = [
-                "FL"
-                "FR"
-              ];
-              "capture.props" = {
-                "node.name" = "sink.upmix_4_0";
-                "media.class" = "Audio/Sink";
-              };
-              "playback.props" = {
-                "node.name" = "playback.upmix-4.0";
-                "audio.position" = [
-                  "FL"
-                  "FR"
-                  "RL"
-                  "RR"
-                ];
-                "target.object" = "alsa_output.pci-0000_c4_00.6.analog-surround-40";
-                "stream.dont-remix" = true;
-                "node.passive" = true;
-                "channelmix.upmix" = true;
-                "channelmix.upmix-method" = "simple";
-              };
-            };
-          }
-        ];
-      };
-    };
+    # try fix sub-wuffer
+    # extraConfig.pipewire-pulse = {
+    #   "99-speaker-routing" = {
+    #     "context.modules" = [
+    #       {
+    #         "name" = "libpipewire-module-loopback";
+    #         "args" = {
+    #           "node.description" = "Stereo to 4.0 upmix";
+    #           "audio.position" = [
+    #             "FL"
+    #             "FR"
+    #           ];
+    #           "capture.props" = {
+    #             "node.name" = "sink.upmix_4_0";
+    #             "media.class" = "Audio/Sink";
+    #           };
+    #           "playback.props" = {
+    #             "node.name" = "playback.upmix-4.0";
+    #             "audio.position" = [
+    #               "FL"
+    #               "FR"
+    #               "RL"
+    #               "RR"
+    #             ];
+    #             "target.object" = "alsa_output.pci-0000_c4_00.6.analog-surround-40";
+    #             "stream.dont-remix" = true;
+    #             "node.passive" = true;
+    #             "channelmix.upmix" = true;
+    #             "channelmix.upmix-method" = "simple";
+    #           };
+    #         };
+    #       }
+    #     ];
+    #   };
+    # };
 
     # If you want to use JACK applications, uncomment this
     jack.enable = true;
