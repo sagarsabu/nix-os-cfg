99cc46f762e08c1226715518909a7795a9f17420
Sagar Sabu <chelekadusagar@gmail.com>
2025-08-24 18:00:02 +1200

    add app image runner

--- asus-zen-s16/global-programs.nix
+++ asus-zen-s16/global-programs.nix
@@ -1,46 +1,52 @@
 { ... }:
 {
   # globally configured programs
+  programs = {
+    firefox.enable = true;
+    xwayland.enable = true;
+    nix-ld.enable = true;
 
-  programs.firefox.enable = true;
-  programs.xwayland.enable = true;
-  programs.nix-ld.enable = true;
+    git = {
+      enable = true;
+      lfs.enable = true;
+    };
 
-  programs.git = {
-    enable = true;
-    lfs.enable = true;
-  };
+    # fish
+    fish = {
+      enable = true;
+      interactiveShellInit = ''
+        # Disable greeting
+        set fish_greeting
+        # enable work bash aliases
+        source /home/sagar/.work_bash_aliases.fish
+      '';
+    };
 
-  # fish
-  programs.fish = {
-    enable = true;
-    interactiveShellInit = ''
-      # Disable greeting
-      set fish_greeting
-      # enable work bash aliases
-      source /home/sagar/.work_bash_aliases.fish
-    '';
-  };
+    # neovim
+    neovim = {
+      enable = true;
+      defaultEditor = true;
+      vimAlias = true;
+    };
 
-  # neovim
-  programs.neovim = {
-    enable = true;
-    defaultEditor = true;
-    vimAlias = true;
-  };
+    openvpn3.enable = true;
 
-  programs.openvpn3.enable = true;
+    steam = {
+      enable = true;
+      remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+      dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+      localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
+    };
 
-  programs.steam = {
-    enable = true;
-    remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
-    dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
-    localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
-  };
+    # wireshark
+    wireshark = {
+      enable = true;
+      dumpcap.enable = true;
+    };
 
-  # wireshark
-  programs.wireshark = {
-    enable = true;
-    dumpcap.enable = true;
+    appimage = {
+      enable = true;
+      binfmt = true;
+    };
   };
 }
